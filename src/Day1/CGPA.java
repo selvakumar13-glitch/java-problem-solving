700d3009470ab877076fc38ebfdc09d4fe8e6678
SELVAKUMAR S <senthilselva2006@gmail.com>
2026-07-09 10:17:18 +0530

    This Program Coding For Day1

---
+++ src/Day1/CGPA.java
@@ -0,0 +1,10 @@
+package Day1;
+public class CGPA {
+
+    public static void main(String[] args)
+    {
+        int age =19;
+        double cgpa = 8.4;
+        System.out.println("My age is "+ age + " and cgpa is " + cgpa);
+    }
+}
---
+++ src/Day1/Details.java
@@ -0,0 +1,11 @@
+package Day1;
+public class Details {
+    public static void main(String[] args)
+    {
+        char initial = 'S';
+        String name = "SELVAKUMAR";
+        int age =19;
+        double cgpa = 8.4;
+        System.out.println(" My name is " + name +" ." + initial + " of age  " + age + " with the cgpa of " + cgpa);
+    }
+}
